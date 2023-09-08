c63a6bc53829a01f5b0ddba7a22202329e195515
Kaveh Asgari <kaveh.nilkaas@gmail.com>
2023-09-08 21:31:33 +0330

    [style]: improvements  with responsive styles

--- src/Components/Home/Introduction.tsx
+++ src/Components/Home/Introduction.tsx
@@ -10,14 +10,14 @@ export default function Introduction() {
         <img src={logo} alt="logo" className='w-24 md:w-28 p-2' />
       </Link>
 
-      <span className='mt-[-32px] sm:mt-0 text-greyText text-center block text-lg sm:text-xl'>سلام ! ما تیم کنترل ای هستیم{'\u2728'}</span>
+      <span className='sm-mt-[-32px] sm:mt-0 text-greyText text-center block text-xl sm:text-2xl'>سلام ! ما تیم کنترل ای هستیم{'\u2728'}</span>
 
       <h1 className='sm:mt-4 text-greyText text-center max-w-lg mx-auto text-3xl sm:text-4xl  leading-[50px] md:leading-[60px] font-bold bg-gradient-to-r from-greyText to-lightBlack bg-clip-text text-fill-transparent'>
         طراحی و توسعه تخصصی وبسایت های کاربردی , قدمی رو به جلو در بازاریابی کسب و کار شما
       </h1>
 
       <Link to={"/about"}>
-        <Button variant="contained" size='large' startIcon={<OpenInNewRoundedIcon className='ml-3' />} className='bg-lightBlack text-greyText border-greyText hover:border-lightText md:text-lg '>اطلاعات بیشتر</Button>
+        <Button variant="contained" size='large' startIcon={<OpenInNewRoundedIcon className='ml-3' />} className='bg-lightBlack text-greyText border-greyText hover:border-lightText text-lg md:text-xl '>اطلاعات بیشتر</Button>
       </Link>
     </div>
   )
