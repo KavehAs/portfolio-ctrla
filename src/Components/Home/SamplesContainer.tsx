486e0deec523eeb05576eca2cf27be446e63fec9
Kaveh Asgari <kaveh.nilkaas@gmail.com>
2023-09-23 13:21:38 +0330

    [app]: add error handler and message

--- src/Components/Home/SamplesContainer.tsx
+++ src/Components/Home/SamplesContainer.tsx
@@ -4,6 +4,7 @@ import SampleCards from "./SampleCards";
 import { Grid } from "@mui/material";
 import { useEffect } from "react";
 import Loading from "../Loading";
+import SentimentDissatisfiedOutlinedIcon from '@mui/icons-material/SentimentDissatisfiedOutlined';
 
 interface websiteDataType {
     id: string,
@@ -19,20 +20,30 @@ export default function SamplesContainer() {
     const { loading, data, error } = useQuery(GET_WEBSITES_INFO);
 
     if (loading) {
-    return (
-        <>
-            <div className="w-full flex justify-center md:hidden">
-                <Loading type={"bubbles"} color='#BDBDBD' width={130} height={250} />
-            </div>
+        return (
+            <>
+                <div className="w-full flex justify-center md:hidden">
+                    <Loading type={"bubbles"} color='#BDBDBD' width={130} height={250} />
+                </div>
 
-            <div className="w-full hidden justify-center md:flex">
-                <Loading type={"bubbles"} color='#BDBDBD' width={200} height={250} />
-            </div>
-        </>
-    )
+                <div className="w-full hidden justify-center md:flex">
+                    <Loading type={"bubbles"} color='#BDBDBD' width={200} height={250} />
+                </div>
+            </>
+        )
     }
 
-    if (!loading) {
+    if (error) {
+        return (
+            <div className="flex flex-col items-center gap-11">
+                <SentimentDissatisfiedOutlinedIcon fontSize="inherit" className="text-pureWhite text-7xl md:text-9xl" />
+                <div className="text-greyText text-2xl md:text-3xl  text-center">
+                    متاسفیم! در دریافت اطلاعات به مشکل برخوردیم... لطفا مجددا امتحان کنید.
+                </div>
+            </div>
+        )
+    }
+    else if (!loading) {
         return (
             <div className="text-greyText">
                 <Grid container>
